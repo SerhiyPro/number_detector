ad56a0c9e11543b7c6fd6976db3e7e47aef56136
SerhiyPro <prots.sergiy7@gmail.com>
2020-02-26 12:31:22 +0200

    add activation and query functions

--- app/neural_network.py
+++ app/neural_network.py
@@ -1,3 +1,7 @@
+import numpy
+import scipy.special
+
+
 class NeuralNetwork:
     # TODO: use dataclasses
     def __init__(self, *args, **kwargs):
@@ -5,9 +9,22 @@ class NeuralNetwork:
         self.hidden_nodes_number = kwargs.get('hidden_nodes_number', 0)
         self.output_nodes_number = kwargs.get('output_nodes_number', 0)
         self.learning_factor = kwargs.get('learning_factor', 0)
+        self.w_input_hidden = kwargs.get('w_input_hidden')
+        self.w_hidden_output = kwargs.get('w_hidden_output')
 
     def train(self):
         pass
 
-    def query(self):
-        pass
+    def query(self, input_layer: list):
+        hidden_inputs = numpy.dot(self.w_input_hidden, input_layer)  # X_hidden = W_inputHidden * I
+        hidden_outputs = self.activation_function(hidden_inputs)  # O_hidden = sigmoid(X_hidden)
+
+        final_inputs = numpy.dot(self.w_hidden_output, hidden_outputs)  # X_final = W_hiddenOutput * O_hidden
+        return self.activation_function(final_inputs)  # O_final = sigmoid(X_final)
+
+    @staticmethod
+    def activation_function(x) -> float:
+        """
+        Sigmoid function (1/1+e^(-x))
+        """
+        return scipy.special.expit(x)
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import numpy
 from app.neural_network import NeuralNetwork
 
 
@@ -7,6 +8,19 @@ if __name__ == '__main__':
     output_nodes_number = 3
 
     learning_factor = 0.3
+    # generating (hidden_nodes_number x input_nodes_number) matrix of
+    # normally distributed random values (from -0.5 to 0.5)
+    normal_distribution_center = 0.0
+    w_input_hidden = numpy.random.normal(normal_distribution_center, pow(hidden_nodes_number, -0.5),
+                                         (hidden_nodes_number, input_nodes_number))
+    # generating (output_nodes_number x hidden_nodes_number) matrix of
+    # normally distributed random values (from -0.5 to 0.5)
+    w_hidden_output = numpy.random.normal(normal_distribution_center, pow(output_nodes_number, -0.5),
+                                          (output_nodes_number, hidden_nodes_number))
 
     n = NeuralNetwork(input_nodes_number=input_nodes_number, hidden_nodes_number=hidden_nodes_number,
-                      output_nodes_number=output_nodes_number, learning_factor=learning_factor)
+                      output_nodes_number=output_nodes_number, learning_factor=learning_factor,
+                      w_input_hidden=w_input_hidden, w_hidden_output=w_hidden_output)
+
+    print(n.w_input_hidden, n.w_hidden_output)
+    print(n.query([1.0, 0.5, -1.5]))
